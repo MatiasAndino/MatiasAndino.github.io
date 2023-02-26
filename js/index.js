3ef9393e403e5c2972c3e388539bb41fd07ed53a
Matias Andino <matiasandinoma@gmail.com>
2023-02-26 18:11:46 -0300

    # Changes to be committed: #    modified:   css/estilo.css #    modified:   index.html #    modified:   js/index.js #

--- js/index.js
+++ js/index.js
@@ -2,35 +2,59 @@ const foto = document.querySelector('#foto')
 const nombre = document.querySelector('#nombre')
 const correo = document.querySelector('#correo')
 const telefono = document.querySelector('#telefono')
+const sobremiNombre = document.querySelector('#sobremiNombre')
+const nacionalidad = document.querySelector('#nacionalidad')
+const direccion = document.querySelector('#direccion')
+const date = document.querySelector('#date')
+const edad = document.querySelector('#edad')
 
-async function newUser() {
+async function nuevoUsuario() {
     try {
         const url = 'https://randomuser.me/api/'
         const respuesta = await fetch(url)
         const {results} = await respuesta.json()
         const datos = results[0]
-        console.log(datos)
+        
         foto.src = datos.picture.large;
-        nombre.textContent = `${datos.name.first} ${datos.name.last}`
+        
+        nombre.textContent = obtenerNombre(datos.name)
+        
+        sobremiNombre.textContent = obtenerNombre(datos.name)
+        
+        nacionalidad.textContent = datos.location.country
+
+        direccion.textContent = obtenerDireccion(datos.location) 
+        
+        date.textContent = obtenerFecha(datos.dob.date)
+        
+        edad.textContent = datos.dob.age
+        
         correo.textContent = datos.email
-        telefono.textContent = datos.phone
+        
+        telefono.textContent = datos.phone        
+
+        console.log(datos)
+
     } catch (e) {
         console.log(e)
     }
 }
-// newUser()
-document.addEventListener('DOMContentLoaded', newUser)
 
-// const newUser = async() => {
-//     try {
-//         const url = 'https://randomuser.me/api/'
-//         const respuesta = await fetch(url)
-//         const {results} = await respuesta.json()
-//         const datos = results[0]
-//         console.log(datos)
-        
-//     } catch (e) {
-//         console.log(e)
-//     }
-// }
+function obtenerFecha (string) {
 
+    const fechaCompleta = new Date(string)
+
+    return `${fechaCompleta.getDate()}/${fechaCompleta.getMonth()}/${fechaCompleta.getFullYear()}`
+}
+
+function obtenerDireccion (objeto) {
+
+    return `${objeto.street.name} ${objeto.street.number}, ${objeto.state}`
+}
+
+function obtenerNombre (objeto) {
+
+    return `${objeto.first} ${objeto.last}`
+}
+
+document.addEventListener('DOMContentLoaded', nuevoUsuario)
